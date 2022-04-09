14dd52f4f7ea60a031affc7153a1d6d788bf482a
Nagy Benedek <nagy.benedek96@gmail.com>
2022-04-09 19:40:01 +0200

    no message

--- f1-application-frontend/src/environments/environment.prod.ts
+++ f1-application-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  title: 'FormulaOne',
+  baseUrl: 'http://localhost:8090/app',
+  userUrl: 'http://localhost:8090/app/user',
+  formulaTeamUrl: 'http://localhost:8090/app/team',
 };
